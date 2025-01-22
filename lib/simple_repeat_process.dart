c4959cb7d8659aae44e0c9dab4511f7fb33b859f
Carlos Eduardo Leite de Andrade <cadu@caduandrade.net>
2025-01-22 17:51:24 -0300

    Updating

--- lib/simple_repeat_process.dart
+++ lib/simple_repeat_process.dart
@@ -7,7 +7,7 @@ void main() async {
       event: _a,
       start: 1,
       name: 'A',
-      interval: Interval.fixed(fixedInterval: 2, untilTime: 6));
+      interval: Interval.fixed(fixedInterval: 2, untilTime: 5));
 
   await sim.run();
 }
