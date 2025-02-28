ad127fbc9af402a4b83a9526ae3f44d75a059918
Carlos Eduardo Leite de Andrade <cadu@caduandrade.net>
2025-02-28 20:00:14 -0300

    Updating

--- lib/limited_resource.dart
+++ lib/limited_resource.dart
@@ -3,7 +3,7 @@ import 'package:simdart/simdart.dart';
 void main() async {
   final SimDart sim = SimDart();
 
-  sim.resources.limited(id: 'resource', capacity: 1);
+  sim.resources.limited(name: 'resource', capacity: 1);
 
   sim.process(event: _eventResource, name: 'A');
   sim.process(event: _eventResource, name: 'B');
